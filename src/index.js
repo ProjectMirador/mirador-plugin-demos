de221cb29bf958ea9419331fa1af2d3d17a842ee
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-03-26 15:56:45 +0100

    implement a copy window plugin

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import mirador from '/home/mathias/github/mirador';
 import config from './mirador-config';
 import downloadImagePlugin from './plugins/download-image';
 import slideshowPlugin from './plugins/slideshow';
+import copyWindowPlugin from './plugins/copy-window';
 
 mirador.viewer(config, [
   downloadImagePlugin,
   slideshowPlugin,
+  copyWindowPlugin,
 ]);
---
+++ src/plugins/copy-window.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import OpenInNewIcon from '@material-ui/icons/OpenInNewOutlined';
+// the mirador version that is used here is not on npm yet
+import mirador from '/home/mathias/github/mirador';
+import omit from 'lodash/omit';
+
+const CopyWindowComponent = (props) => (
+  <>
+    <IconButton onClick={props.copyWindow}>
+      <OpenInNewIcon />
+    </IconButton>
+  </>
+);
+
+const copyWindowAction = (windowId) => (dispatch, getState) => {
+  const window = getState().windows[windowId];
+  const cleanedWindow = omit(window, [
+    'id',
+    'companionWindowIds',
+    'thumbnailNavigationId',
+  ]);
+  dispatch(mirador.actions.addWindow(cleanedWindow));
+};
+
+const mapStateToProps = (state) => ({
+  state: state,
+});
+
+const mapDispatchToProps = (dispatch, { windowId }) => ({
+  copyWindow: () => dispatch(copyWindowAction(windowId)),
+});
+
+export default {
+  target: 'WindowTopMenu',
+  mode: 'add',
+  component: CopyWindowComponent,
+  mapDispatchToProps: mapDispatchToProps,
+  mapStateToProps: mapStateToProps,
+}
