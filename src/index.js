9f2d45ead9c0ee8f86984b1c730737cdf6793db3
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-03-26 15:31:06 +0100

    fix meaningless import

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import mirador from '/home/mathias/github/mirador';
 import config from './mirador-config';
 import downloadImagePlugin from './plugins/download-image';
 import slideshowPlugin from './plugins/slideshow';
-import copyWindowPlugin from './plugins/copy-window';
 
 mirador.viewer(config, [
   downloadImagePlugin,
   slideshowPlugin,
-  copyWindowPlugin,
 ]);
