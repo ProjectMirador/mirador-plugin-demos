44d74826773c8cc03d9727394513288838165a99
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-04-04 10:50:28 +0200

    some changes to the build environment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
